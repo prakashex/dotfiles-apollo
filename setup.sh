c15b99fb3df96a838061a2c7b6e4a00a8816e92c
Prakash <prakashope@gmail.com>
2022-06-09 16:20:18 +0530

    initialized script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+pacman -Syu
+
+pacman -S ttf-font-awesome papirus-icon-theme noto-fonts-emoji noto-fonts ttf-fira-code glances rofi telegram-desktop kitty element-desktop obs-studio pycharm-community-edition flameshot deluge git picom nitrogen vim github-cli p7zip polybar
+
+git clone 
+
+## 7z x .fonts.7z
+
+mv -R .fonts/* /usr/share/fonts/
