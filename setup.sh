cb96fd89debee8c0015ad2aada31abad4b4b33cb
Prakash <prakashope@gmail.com>
2022-06-09 16:44:39 +0530

    initialized setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 pacman -Syu
 
-pacman -S ttf-font-awesome papirus-icon-theme noto-fonts-emoji noto-fonts ttf-fira-code glances rofi telegram-desktop kitty element-desktop obs-studio pycharm-community-edition flameshot deluge git picom nitrogen vim github-cli p7zip polybar
+pacman -S ttf-font-awesome papirus-icon-theme noto-fonts-emoji noto-fonts ttf-fira-code glances rofi telegram-desktop kitty element-desktop obs-studio pycharm-community-edition flameshot deluge git picom nitrogen vim github-cli p7zip polybar nemo
 
 git clone 
 
 ## 7z x .fonts.7z
 
 mv -R .fonts/* /usr/share/fonts/
+
+
+## nemo setup 
+
+wget https://raw.githubusercontent.com/mhsattarian/nemo-open-in-vscode/master/vscode.nemo_action -O ~/.local/share/nemo/actions/vscode.nemo_action
+
+
+gsettings set org.cinnamon.desktop.default-applications.terminal exec xfce4-terminal
+
+
