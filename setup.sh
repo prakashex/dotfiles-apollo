1aeeac50b4d768e513c1ca3d2b82afb2dc43f982
Prakash <prakashope@gmail.com>
2022-07-02 16:23:29 +0530

    removed brave , added custom picom source.
    added nvidia install instructions.

--- setup.sh
+++ setup.sh
@@ -4,19 +4,24 @@ shopt -s expand_aliases
 
 sudo pacman -Syu
 
-sudo pacman -S ttf-font-awesome papirus-icon-theme noto-fonts-emoji noto-fonts ttf-fira-code glances rofi telegram-desktop alacritty element-desktop obs-studio  flameshot deluge-gtk git nitrogen vim github-cli p7zip polybar nemo libarchive xpdf
+sudo pacman -S ttf-font-awesome papirus-icon-theme noto-fonts-emoji noto-fonts ttf-fira-code htop glances rofi telegram-desktop alacritty element-desktop obs-studio  flameshot deluge-gtk git nitrogen vim github-cli p7zip polybar nemo libarchive xpdf nvidia-installer-dkms
 
 ## change default image pdf viewer and image viewer
 
 ## dev tools
 ## install custom package for picom
 ## install nvidia drivers
+## https://github.com/pijulius/picom
 
+## install nvidia stuff.
 
+## sudo pacman -S nvidia-installer-dkms -- this package is already installed above.
+## sudo nvidia-installer-dkms
+## sudo systemctl reboot
 sudo pacman -S pycharm-community-edition intellij-idea-community-edition jdk-openjdk nodejs npm python
 
 
-yay -S freedownloadmanager lens-bin zoom brave-bin visual-studio-code-bin google-chrome --noconfirm
+yay -S freedownloadmanager lens-bin zoom visual-studio-code-bin google-chrome --noconfirm
 ## nemo setup 
 
 
