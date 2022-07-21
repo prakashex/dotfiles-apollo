eba567b32d7fd161964af9721c9d008161325ab6
Prakash <prakashope@gmail.com>
2022-07-21 23:27:44 +0530

    fixed temperature module

--- .config/polybar/scripts/launch-polybar.sh
+++ .config/polybar/scripts/launch-polybar.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
+
+
+
 # Terminate already running bar instances
 killall -q polybar
 
