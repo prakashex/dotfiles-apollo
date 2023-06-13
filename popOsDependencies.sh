4c02ee83fbe01d0ae246d20b7e603e71b8658238
Prakash <prakashope@gmail.com>
2023-06-13 23:25:20 +0530

    picom build dependency update

--- popOsDependencies.sh
+++ popOsDependencies.sh
@@ -62,7 +62,7 @@ sudo mv /usr/share/themes/gtk-master/ /usr/share/themes/dracula
 
 # install picom build dependencies
 
-sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
+sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libpcre3-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev
 
 # picom extra dependency on ubuntu server
 # commented the below code because a separate branch for ubuntu server was created
