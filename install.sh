e1ae20abb8fc3197a26e3e637918475077c5222e
Prakash <prakashope@gmail.com>
2023-08-26 10:54:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ sudo apt upgrade
 
 ## install polybar rofi nitrogen lxappearance acpi
 
-sudo apt install polybar rofi flameshot transmission-gtk telegram-desktop mpv htop vim git nitrogen lxappearance libarchive-tools nemo xfce4-terminal gnome-tweaks intel-media-va-driver-non-free regolith-i3-workspace-config python-is-python3
+sudo apt install polybar rofi flameshot transmission-gtk telegram-desktop mpv htop feh gh python3 python3-pip vim git nitrogen lxappearance libarchive-tools nemo xfce4-terminal gnome-tweaks intel-media-va-driver-non-free regolith-i3-workspace-config python-is-python3
 
 
 ## install postman
