770cae612120bdbf432540d2a747c2cc94ca60b7
Prakash <qxprakash@gmail.com>
2024-07-21 18:54:23 +0530

    polybar support for multimonitors

--- .config/polybar/scripts/launch-polybar.sh
+++ .config/polybar/scripts/launch-polybar.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+# Find and set HWMON_PATH for coretemp
 for i in /sys/class/hwmon/hwmon*/temp*_input; do 
     if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
         export HWMON_PATH="$i"
     fi
 done
-
-
 
 # Terminate already running bar instances
 killall -q polybar
@@ -14,9 +13,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar
-polybar example &
+# Launch Polybar on all connected monitors
+if type "xrandr" > /dev/null; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
+# Lower Polybar window after a short delay
 sleep 1
 xdo lower -N Polybar
-
